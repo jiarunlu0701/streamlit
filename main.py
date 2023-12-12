22f9e64a4e6749dad6ab6afc03bde95c32b68ec8
jiarunlu0701 <jiarun.lu@newsbreak.com>
2023-12-12 11:13:26 +0800

    update requirement

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import streamlit as st
+import streamlit_authenticator as stauth
+import yaml
+from yaml.loader import SafeLoader
 from PIL import Image
 from db import db
 from utils import utils
@@ -9,6 +12,27 @@ db = db()
 utils = utils()
 generate = generate()
 count = count()
+
+with open('../config.yaml') as file:
+    config = yaml.load(file, Loader=SafeLoader)
+
+authenticator = stauth.Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days'],
+    config['preauthorized']
+)
+
+authenticator.login('Login', 'main')
+if st.session_state["authentication_status"]:
+    authenticator.logout('Logout', 'main', key='unique_key')
+    st.write(f'Welcome *{st.session_state["name"]}*')
+    st.title('Some content')
+elif st.session_state["authentication_status"] is False:
+    st.error('Username/password is incorrect')
+elif st.session_state["authentication_status"] is None:
+    st.warning('Please enter your username and password')
 
 image = Image.open('assets/sap.png')
 prompt_tokens = 0
@@ -80,6 +104,7 @@ with st.sidebar:
         db.delete_chat_history()
         count.reset_token_usage()
         st.success('Chat history and token counts cleared!')
+
 
 if page == "Chat":
     chat_page()
